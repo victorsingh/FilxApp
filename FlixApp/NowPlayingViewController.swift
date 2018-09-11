9dcfaafa5cc3b8b7ae904d5c882dc7ffdfd688bf
Victor Singh <victor.singh03@qmail.cuny.edu>
2018-09-11 08:06:49 -0400

    User can Pull to refresh the movie list

--- FlixApp/NowPlayingViewController.swift
+++ FlixApp/NowPlayingViewController.swift
@@ -14,12 +14,24 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector (NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
-        
+        fetchMovies()
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func didPullToRefresh (_ refreshControl: UIRefreshControl){
+        fetchMovies()
+    }
+    
+    func fetchMovies() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")! //! is used to unwrapthis if you dont get a proper response
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main )
@@ -32,14 +44,14 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 self.movies = movies
                 self.tableView.reloadData()
-//                for movie in movies {
-//                    let title = movie["title"] as! String
-//                    print(title)
-//                }
+                self.refreshControl.endRefreshing()
+                //                for movie in movies {
+                //                    let title = movie["title"] as! String
+                //                    print(title)
+                //                }
             }
         }
         task.resume()
-        // Do any additional setup after loading the view.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
