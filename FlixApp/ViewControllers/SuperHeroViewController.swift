7e3975aa636ca4bb592e86f9123fb07ca5ed2916
Victor Singh <victor.singh03@qmail.cuny.edu>
2018-09-17 14:56:58 -0400

    User can tap a tab bar button to view a grid layout of Movie Posters using a CollectionView.

---
+++ FlixApp/ViewControllers/SuperHeroViewController.swift
@@ -0,0 +1,89 @@
+//
+//  SuperHeroViewController.swift
+//  FlixApp
+//
+//  Created by Victor Singh on 9/17/18.
+//  Copyright © 2018 victor. All rights reserved.
+//
+
+import UIKit
+
+class SuperHeroViewController: UIViewController, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    var movies: [[String: Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewLayout as! UICollectionViewFlowLayout
+        // Do any additional setup after loading the view.
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = layout.minimumInteritemSpacing
+        let cellsPerLine: CGFloat = 4
+        let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerLine - 1)
+        let width = collectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / cellsPerLine
+        layout.itemSize = CGSize(width: width, height: width * 3/2)
+        fetchMovies()
+    }
+    
+    func fetchMovies() {
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/363088/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")! //! is used to unwrapthis if you dont get a proper response
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main )
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(dataDictionary)
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                
+                self.collectionView.reloadData()
+//                self.refreshControl.endRefreshing()
+                }
+            }
+        
+        
+        task.resume()
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
+        
+        let movie = movies[indexPath.item]
+        
+        if let posterPathString = movie["poster_path"] as? String {
+            let baseURLString = "https://image.tmdb.org/t/p/w500"
+            let posterURL = URL(string: baseURLString + posterPathString)!
+            cell.posterImageView.af_setImage(withURL: posterURL)
+        }
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FlixApp/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  FlixApp
+//
+//  Created by Victor Singh on 9/17/18.
+//  Copyright © 2018 victor. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
